abef97d69d1c680c986ff859c0e006028cfbdaa2
Félix Desjardins <felixinx@gmail.com>
2021-01-08 08:40:00 -0500

    Download page update

--- app/Http/Livewire/DownloadsList.php
+++ app/Http/Livewire/DownloadsList.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Period;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class DownloadsList extends Component
 {
     public $directories;
-    public $selectedDirectory = '';
-    public $files = [];
-
-    public function setSelectedDirectory(string $selectedDirectory)
-    {
-        if (!in_array($selectedDirectory, $this->directories)) return;
-
-        $this->selectedDirectory = $selectedDirectory;
-        $this->files = Storage::files($selectedDirectory);
-    }
+    public $period;
 
     public function mount()
     {
         $this->directories = Storage::directories('public/archive');
+        $this->period = Period::whereDate('start_date', '<=', today())->whereDate('end_date', '>=', today())->first();
     }
 }
